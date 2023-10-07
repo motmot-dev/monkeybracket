3a55d7e27e007310320a2ce0c5d213e3b34bb512
ianreas <muhammedik111101@gmail.com>
2023-10-07 16:16:47 -0400

    ian commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  optimizeFonts:true,
+
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        
+      },
+    ],
+    minimumCacheTTL:1500000,
+  },
+};
 
 module.exports = nextConfig
